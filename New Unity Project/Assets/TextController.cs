a0a8e0587145234c940a1174bae6033a376190c8
Wes Sparks <wesdirect@gmail.com>
2016-11-12 10:44:02 -0700

    First Class Section

---
+++ New Unity Project/Assets/TextController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = "";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Space)){
+			text.text = "Space Pressed";
+		}
+		
+	}
+}
